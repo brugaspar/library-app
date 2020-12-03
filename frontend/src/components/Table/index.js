2fe5a95120163a6cc7bad9189fdd55c0b3b95f32
Bruno Gaspar <bruninhoogaspar@gmail.com>
2020-12-03 20:10:38 -0300

    feat: finished frontend

--- frontend/src/components/Table/index.js
+++ frontend/src/components/Table/index.js
@@ -1,11 +1,33 @@
-import { FiTrash2, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash2, FiCheckSquare, FiXCircle } from 'react-icons/fi'
 import { Table } from 'reactstrap';
+
+import api from '../../services/api';
 
 import './styles.css';
 
-const TableData = ({ books }) => {
+const TableData = ({ books, loadBooks }) => {
+  const handleUpdateStatus = async (id, status) => {
+    let statusId = 1;
+
+    if (status === 1) {
+      statusId = 2;
+    }
+
+    await api.put(`books/${id}`, {
+      statusId
+    });
+
+    loadBooks();
+  }
+
+  const handleDeleteBook = async (id) => {
+    await api.delete(`books/${id}`);
+
+    loadBooks();
+  }
+
   return (
-    <Table bordered>
+    <Table bordered striped>
       <thead className="thead-dark">
         <tr>
           <th>#</th>
@@ -27,17 +49,30 @@ const TableData = ({ books }) => {
             <td>{book.updated_at}</td>
             <td>{book.status_name}</td>
             <td align="center">
-              <FiCheckSquare
-                size={20}
-                color="#16a085"
-                className="fi-check"
-              />
+              <button className="table-button" onClick={() => handleUpdateStatus(book.id, book.status_id)}>
+                {book.status_id === 1
+                  ? (
+                    <FiXCircle
+                      size={20}
+                      color="#ca59cb"
+                    />
+                  )
+                  : (
+                    <FiCheckSquare
+                      size={20}
+                      color="#16a085"
+                    />
+                  )
+                }
+              </button>
 
-              <FiTrash2
-                size={20}
-                color="#c0392b"
-                className="fi-trash"
-              />
+              <button className="table-button" onClick={() => handleDeleteBook(book.id)}>
+                <FiTrash2
+                  size={20}
+                  color="#c0392b"
+                  className="fi-trash"
+                />
+              </button>
             </td>
           </tr>
         ))}
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -36,28 +36,43 @@ const Home = () => {
   const handleAuthorRegister = async (event) => {
     event.preventDefault();
 
-    await api.post('authors', { name: authorName });
+    try {
+      await api.post('authors', { name: authorName });
 
-    loadAuthors();
-    authorToggle();
+      loadAuthors();
+      authorToggle();
 
-    reset();
+      reset();
+    } catch {
+      alert('Algo de errado não está certo. Tente novamente!');
+
+      reset();
+    }
   }
 
   const handleBookRegister = async (event) => {
     event.preventDefault();
 
-    const response = await api.post('books', {
-      title: bookTitle,
-      authorId,
-      statusId
-    });
-    console.log(response.data)
+    try {
+      if (authorId === 0 || statusId === 0) {
+        throw new Error;
+      }
 
-    loadBooks();
-    bookToggle();
+      await api.post('books', {
+        title: bookTitle,
+        authorId,
+        statusId
+      });
 
-    reset();
+      loadBooks();
+      bookToggle();
+
+      reset();
+    } catch {
+      alert('Algo de errado não está certo. Tente novamente!');
+
+      reset();
+    }
   }
 
   const loadAuthors = async () => {
@@ -99,11 +114,11 @@ const Home = () => {
       </div>
 
       <div className="home-content">
-        <Table books={books} />
+        <Table books={books} loadBooks={loadBooks} />
       </div>
 
 
-      <Modal isOpen={authorModal} toggle={authorToggle}>
+      <Modal isOpen={authorModal} toggle={authorToggle} autoFocus={false}>
         <ModalHeader toggle={authorToggle}>
           <h2>Cadastro de autor</h2>
         </ModalHeader>
@@ -116,64 +131,69 @@ const Home = () => {
               placeholder="Nome"
               className="form-control"
               required
+              autoFocus={true}
               value={authorName}
               onChange={event => setAuthorName(event.target.value)}
             />
             <ModalFooter>
               <button className="btn btn-success" type="submit">Cadastrar</button>{' '}
-              <button className="btn btn-danger" onClick={authorToggle}>Cancelar</button>
+              <button className="btn btn-danger" type="button" onClick={authorToggle}>Cancelar</button>
             </ModalFooter>
           </form>
         </ModalBody>
       </Modal>
 
-      <Modal isOpen={bookModal} toggle={bookToggle}>
+      <Modal isOpen={bookModal} toggle={bookToggle} autoFocus={false}>
         <ModalHeader toggle={bookToggle}>
           <h2>Cadastro de livro</h2>
         </ModalHeader>
         <ModalBody className="modal-body">
-          <label className="modal-label" htmlFor="title">Título do livro</label>
-          <input
-            type="text"
-            id="title"
-            placeholder="Título"
-            className="form-control"
-            value={bookTitle}
-            onChange={event => setBookTitle(event.target.value)}
-          />
-
-          <label className="modal-label" htmlFor="author">Autor do livro</label>
-          <select
-            id="author"
-            className="form-control"
-            value={authorId}
-            onChange={event => setAuthorId(event.target.value)}
-          >
-            <option value="0" disabled>Selecione...</option>
-            {authors.map(author => (
-              <option key={author.id} value={author.id}>{author.name}</option>
-            ))}
-          </select>
-
-          <div className="modal-check">
-            <label className="modal-label check-label" htmlFor="status">Status:</label>
-            <select
-              id="status"
+          <form onSubmit={handleBookRegister}>
+            <label className="modal-label" htmlFor="title">Título do livro</label>
+            <input
+              type="text"
+              id="title"
+              placeholder="Título"
               className="form-control"
-              value={statusId}
-              onChange={event => setStatusId(event.target.value)}
+              required
+              autoFocus={true}
+              value={bookTitle}
+              onChange={event => setBookTitle(event.target.value)}
+            />
+
+            <label className="modal-label" htmlFor="author">Autor do livro</label>
+            <select
+              id="author"
+              className="form-control"
+              value={authorId}
+              onChange={event => setAuthorId(event.target.value)}
             >
               <option value="0" disabled>Selecione...</option>
-              {status.map(st => (
-                <option key={st.id} value={st.id}>{st.name}</option>
+              {authors.map(author => (
+                <option key={author.id} value={author.id}>{author.name}</option>
               ))}
             </select>
-          </div>
+
+            <div>
+              <label className="modal-label" htmlFor="status">Status:</label>
+              <select
+                id="status"
+                className="form-control"
+                value={statusId}
+                onChange={event => setStatusId(event.target.value)}
+              >
+                <option value="0" disabled>Selecione...</option>
+                {status.map(st => (
+                  <option key={st.id} value={st.id}>{st.name}</option>
+                ))}
+              </select>
+            </div>
+            <ModalFooter>
+              <button className="btn btn-success" type="submit">Cadastrar</button>{' '}
+              <button className="btn btn-danger" type="button" onClick={bookToggle}>Cancelar</button>
+            </ModalFooter>
+          </form>
         </ModalBody>
-        <ModalFooter>
-          <button className="btn btn-success" onClick={handleBookRegister}>Cadastrar</button>{' '}
-          <button className="btn btn-danger" onClick={bookToggle}>Cancelar</button>
-        </ModalFooter>
       </Modal>
     </div>
   );
